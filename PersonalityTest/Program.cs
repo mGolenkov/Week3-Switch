01b4f4fc2fe479e0f5efabdb01a0263e053bf5e8
Maksim Golenkov <mgolenko@tktk.ee>
2021-11-19 23:29:04 +0200

    add new projects

---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada tema lemmikvärv;
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "roheline", konsol kuvab "oled looduse sõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik";
+
+            Console.WriteLine("Sissesta oma lemmik värv");
+            string userColor = Console.ReadLine();
+
+            if (userColor == "punane")
+            {
+                Console.WriteLine("oled romantiline.");
+            }
+            else if (userColor == "sinine")
+            {
+                Console.WriteLine("oled töökas.");
+            }
+            else if (userColor == "roheline")
+            {
+                Console.WriteLine("oled looduse sõber.");
+            }
+            else
+            {
+                Console.WriteLine($"oled {userColor} ükssarvik");
+            }
+            Console.WriteLine("Kena päeva!");
+
+         
+
+
+
+                
+
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada tema lemmikvärv;
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "roheline", konsol kuvab "oled looduse sõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik";
+
+            Console.WriteLine("Sissesta oma lemmik värv");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch(userColor)
+            {
+                case "punane":
+                    Console.WriteLine("oled romantiline.");
+                    break;
+                case "sininse":
+                    Console.WriteLine("oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("oled looduse sõber.");
+                    break;
+                default:
+                    Console.WriteLine($"oled {userColor} ükssarvik");
+                    break;
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
